67e35934f7261f832a49f7d5672257d67ecc622d
Rahul Vishwakarma <rahulvs2809@gmail.com>
2024-07-23 18:22:50 +0530

    Update rainbow_grading.spec.js

--- site/cypress/e2e/Cypress-Gradeable/rainbow_grading.spec.js
+++ site/cypress/e2e/Cypress-Gradeable/rainbow_grading.spec.js
@@ -1,11 +1,11 @@
 describe('Test Rainbow Grading', () => {
     beforeEach(() => {
         cy.login('instructor');
-        cy.visit(['sample', 'config']);
+        cy.visit(['tutorial', 'config']);
     });
     it('Enable viewing of rainbow grades and generating the rainbow grading', () => {
         cy.get('[data-testid="display-rainbow-grades-summary"]').check();
-        cy.visit(['sample', 'reports', 'rainbow_grades_customization']);
+        cy.visit(['tutorial', 'reports', 'rainbow_grades_customization']);
         cy.get('[data-testid="display-rainbow-grades-summary"]').should('be.checked');
         cy.get('[data-testid="display-grade-summary"]').check();
         cy.get('[data-testid="display-grade-summary"]').should('be.checked');
@@ -15,7 +15,7 @@ describe('Test Rainbow Grading', () => {
         cy.get('[data-testid="display-benchmarks-perfect"]').check();
         cy.get('[data-testid="save-status-button"]').click();
         cy.get('[data-testid="save-status"]', { timeout: 15000 }).should('contain', 'Rainbow grades successfully generated!');
-        cy.visit(['sample', 'grades']);
+        cy.visit(['tutorial', 'grades']);
         ['USERNAME', 'NUMERIC ID', 'AVERAGE', 'STDDEV', 'PERFECT'].forEach((fields) => {
             cy.get('[data-testid="rainbow-grades"]').should('contain', fields);
         });
@@ -23,7 +23,7 @@ describe('Test Rainbow Grading', () => {
         ['ta', 'student', 'grader', 'instructor'].forEach((username) => {
             cy.logout();
             cy.login(username);
-            cy.visit(['sample', 'grades']);
+            cy.visit(['tutorial', 'grades']);
             cy.get('[data-testid="rainbow-grades"]').should('contain', `Lecture Participation Polls for: ${username}`);
             if (username === 'instructor') {
                 checkRainbowGrades('instructor', 801516157, 'Quinn', 'Instructor');
@@ -42,10 +42,10 @@ describe('Test Rainbow Grading', () => {
                 checkRainbowGradesOption();
             }
         });
-        cy.visit(['sample', 'config']);
+        cy.visit(['tutorial', 'config']);
         cy.get('[data-testid="display-rainbow-grades-summary"]').uncheck();
         cy.get('[data-testid="display-rainbow-grades-summary"]').should('not.be.checked');
-        cy.visit(['sample', 'grades']);
+        cy.visit(['tutorial', 'grades']);
         cy.get('[data-testid="rainbow-grades"]').should('contain', 'No grades are available...');
     });
 });
