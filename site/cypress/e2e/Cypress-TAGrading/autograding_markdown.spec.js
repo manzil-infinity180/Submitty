3c37b604b925b564feb77cbd1d1367fc61f5da80
Rahul Vishwakarma <rahulvs2809@gmail.com>
2024-07-26 10:52:27 +0530

    Update autograding_markdown.spec.js

--- site/cypress/e2e/Cypress-TAGrading/autograding_markdown.spec.js
+++ site/cypress/e2e/Cypress-TAGrading/autograding_markdown.spec.js
@@ -50,3 +50,49 @@ describe('Visual testing 2', () => {
         });
     });
 });
+
+describe('Visual testing 2', () => {
+    it('autograding markdown testing', () => {
+        // cy.viewport(1000, 900);
+        cy.login('instructor');
+        cy.visit(['testing', 'gradeable', 'open_homework', 'grading', 'grade?who_id=BVgjdVcF8tSiwnD&sort=id&direction=ASC']);
+        cy.get('#nav-sidebar-collapse-sidebar').click();
+        cy.get('[data-testid="details-tc-expand-all"]').click();
+        cy.get('body').scrollTo('bottom');
+        cy.get('[data-testid="autograding-results"]').compareSnapshot('autograding_code-4', 0.02, {
+            capture: 'viewport',
+        });
+        cy.get('[data-testid="test-box-block"]').scrollIntoView().should('be.visible');
+        cy.get('[data-testid="test-box-block"]').compareSnapshot('autograding_code-3', 0.02, {
+            capture: 'viewport',
+        });
+    });
+});
+
+describe('Visual testing 2', () => {
+    it('autograding markdown testing', () => {
+        // cy.viewport(1000, 900);
+        cy.login('instructor');
+        cy.visit(['testing', 'gradeable', 'open_homework', 'grading', 'grade?who_id=BVgjdVcF8tSiwnD&sort=id&direction=ASC']);
+        cy.get('#nav-sidebar-collapse-sidebar').click();
+        cy.get('[data-testid="details-tc-expand-all"]').click();
+        cy.get('body').scrollTo('center');
+        cy.get('[data-testid="autograding-results"]').compareSnapshot('autograding_code-5', 0.02, {
+            capture: 'viewport',
+        });
+    });
+});
+
+describe('Visual testing 2', () => {
+    it('autograding markdown testing', () => {
+        // cy.viewport(1000, 900);
+        cy.login('instructor');
+        cy.visit(['testing', 'gradeable', 'open_homework', 'grading', 'grade?who_id=BVgjdVcF8tSiwnD&sort=id&direction=ASC']);
+        cy.get('#nav-sidebar-collapse-sidebar').click();
+        cy.get('[data-testid="details-tc-expand-all"]').click();
+        cy.get('body').scrollTo('center');
+        cy.get('body').compareSnapshot('autograding_code-6', 0.02, {
+            capture: 'viewport',
+        });
+    });
+});
